55b3b1f924f4f57862b9cb50115d2516266ac6ff
giji676 <tvtengoxachidze@gmail.com>
2026-06-13 20:14:53 +0100

    Comments for the giji module

--- security/giji/giji_main.c
+++ security/giji/giji_main.c
@@ -1,13 +1,28 @@
+/*
+ * Giji LSM
+ * 
+ * Adds security hook(s) for LSM.
+ * 
+ * LSM will call the callback funciton provided to the hook,
+ * which in this case will call the Rust function to handle the permission.
+ *
+ */
+
 #include <linux/lsm_hooks.h>
 
+// Implemented in rust
 extern int rust_inode_handler(struct inode *inode, int mask);
 
 static int giji_inode_permission(struct inode *inode, int mask) {
-    return rust_inode_handler(inode, mask);
+	return rust_inode_handler(inode, mask);
 }
 
+// Define hooks
+//
+// Available hooks can be seen at
+//	linux/include/linux/lsm_hook_defs.h
 static struct security_hook_list giji_hooks[] __ro_after_init = {
-    LSM_HOOK_INIT(inode_permission, giji_inode_permission),
+	LSM_HOOK_INIT(inode_permission, giji_inode_permission),
 };
 
 static const struct lsm_id giji_lsmid = {
--- security/giji/giji_rust.rs
+++ security/giji/giji_rust.rs
@@ -3,7 +3,25 @@
 use kernel::prelude::*;
 use kernel::bindings;
 
+// LOG_PREFIX for pr_info! macro
 const __LOG_PREFIX: &[u8] = b"GIJI\0";
+
+/*
+ * Implementing the handler that will be called from the C code
+ * returns 0 if permission is granted
+ * otherwise denies, options can be seen at
+ *     linux/include/uapi/asm-generic/errno-base.h
+ *
+ * To test callback is being received specifically on
+ * inode_handler, do any file relate operations in the kernel,
+ * such as `ls`, `cat file`, which should print the `inode_handler call` log
+ *
+ * TODO: This is unsafe
+ * `kernel::bindings` should not be used here,
+ * instead a safe/sound wrapper should be written in
+ *     linux/rust/kernel/
+ *
+ */
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn rust_inode_handler(
@@ -12,6 +30,6 @@ unsafe extern "C" fn rust_inode_handler(
 ) -> i32 {
     pr_info!("inode_handler call\n");
     0
-    // -(bindings::EPERM as i32)
+        // -(bindings::EPERM as i32)
 }
 
